49469d33f3e24d0a82c40be55bfb11d9b9bc3cd9
吴洪波 <83867099+HongboWu77@users.noreply.github.com>
2022-12-08 16:12:53 +0800

    Add files via upload

---
+++ simple_transaction_back/src/main/java/com/ynu/springboot_demo/controller/OrderController.java
@@ -0,0 +1,73 @@
+package com.ynu.springboot_demo.controller;
+
+import com.ynu.springboot_demo.dao.OrderDao;
+import com.ynu.springboot_demo.entity.CommonResult;
+import com.ynu.springboot_demo.entity.Order;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author xiaobai
+ * @create 2022-10-22-
+ */
+@Api(tags = "订单接口")
+@RestController
+public class OrderController {
+    @Resource
+    private OrderDao orderDao;
+    @Value("${server.port}")
+    private String serverPort;
+
+    @ApiOperation(value = "增加订单信息")
+    @PostMapping("/order")
+    public CommonResult<Order> save(@RequestBody Order order){
+        Order result = orderDao.save(order);
+        return new CommonResult<Order>(200,"插入数据成功，serverPort："+serverPort,order);
+    }
+
+    @ApiOperation(value = "修改订单信息")
+    @PutMapping("/order/{id}")
+    public CommonResult<Order> update(@PathVariable("id")Long id,@RequestBody Order order){
+        Optional<Order> byId = orderDao.findById(id);
+        if(!byId.isPresent()){
+            return new CommonResult<Order>(444,"未查询到该订单，serverPort："+serverPort);
+        }
+        order.setOrderId(id);
+        Order result = orderDao.save(order);
+        return new CommonResult<Order>(200,"修改数据成功，serverPort："+serverPort,result);
+    }
+
+    @ApiOperation(value = "根据Id获取订单信息")
+    @GetMapping("/order/{id}")
+    public CommonResult<Order> findById(@PathVariable("id")Long id){
+        Optional<Order> byId = orderDao.findById(id);
+        if(!byId.isPresent()){
+            return new CommonResult<Order>(444,"未查询到该订单，serverPort："+serverPort);
+        }
+        return new CommonResult<Order>(200,"查询数据成功，serverPort："+serverPort,byId.get());
+    }
+
+    @ApiOperation(value = "获取全部订单信息")
+    @GetMapping("/order")
+    public CommonResult<List<Order>> findAll(){
+        List<Order> all = orderDao.findAll();
+        return new CommonResult<List<Order>>(200,"查询数据成功，serverPort："+serverPort,all);
+    }
+
+    @ApiOperation(value = "删除订单信息")
+    @DeleteMapping("/order/{id}")
+    public CommonResult<Long> delete(@PathVariable("id")Long id){
+        Optional<Order> byId = orderDao.findById(id);
+        if(!byId.isPresent()){
+            return new CommonResult<Long>(444,"未查询到该订单，serverPort："+serverPort);
+        }
+        orderDao.deleteById(id);
+        return new CommonResult<Long>(200,"删除数据成功，serverPort："+serverPort,id);
+    }
+}
---
+++ simple_transaction_back/src/main/java/com/ynu/springboot_demo/entity/User.java
@@ -0,0 +1,34 @@
+package com.ynu.springboot_demo.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+/**
+ * @author xiaobai
+ * @create 2022-10-22-
+ */
+@Entity
+//User实体类对应的表名
+@Table(name = "user")
+//lombok插件实现了getter、setter、有参无参构造、toString等
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    //主键定义
+    @Id
+    //主键生成策略：每个表单独计算
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //对应的表中的字段名
+    @Column(name = "user_id")
+    private Long userId;
+    @Column(name = "user_name")
+    private String userName;
+    @Column(name = "user_sex")
+    private String userSex;
+    @Column(name = "order_id")
+    private String orderId;
+    @Column(name = "user_password")
+    private String userPassword;
+}
